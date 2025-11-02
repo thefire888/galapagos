48accd7db29728c28573ea97d915f8514cbcddf6
Luan Leal <luanric888@gmail.com>
2025-11-02 13:21:40 -0300

    new examples

--- examples/simulate_drift.py
+++ examples/simulate_drift.py
@@ -1,12 +1,12 @@
 from galapagos.simulation import Simulation
 from galapagos.locus import Locus
 
-genepool = [(Locus(("A", "A")), 1.1),
+genepool = [(Locus(("A", "A")), 1.0),
             (Locus(("A", "a")), 1.0),
             (Locus(("a", "a")), 1.0)
             ]
 simulation = Simulation(max_generations=150,
-                        population_size=200,
+                        population_size=20,
                         genepool=genepool
                         )
 
