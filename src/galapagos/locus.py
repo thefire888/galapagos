bc2267472796dc798b042ee5724274dc890d755f
Luan Leal <luanric888@gmail.com>
2025-11-03 10:27:19 -0300

    update
    now locus __str__ is fully implemented
    
    added new genepool class, this is unstable!

--- src/galapagos/locus.py
+++ src/galapagos/locus.py
@@ -8,7 +8,10 @@ class Locus:
 
     def __str__(self):
         canon_alleles = sorted(self.alleles)
-        return f"{canon_alleles[0]}{canon_alleles[1]}"
+        return_str = f""
+        for i in canon_alleles:
+            return_str += f"{i}"
+        return return_str
 
     def __eq__(self, other: Self) -> bool:
         return set(self.alleles) == set(other.alleles)
