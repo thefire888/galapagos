d6b7655adcbefcaf8e8107a31d5007653314ad1b
Luan Leal <luanric888@gmail.com>
2025-12-03 14:57:28 -0300

    same problem, it NEEDS the genepool to function

--- tests/simulate_selection.py
+++ tests/simulate_selection.py
@@ -6,26 +6,30 @@ from galapagos.generation import Generation
 from galapagos.utils import Utils
 
 genepool = [
-                (Locus(('A', 'A')), 0.98),
-                (Locus(('A', 'a')), 0.99),
+                (Locus(('A', 'A')), 0.50),
+                (Locus(('A', 'a')), 0.50),
                 (Locus(('a', 'a')), 1.0)
               ]
 
 genotype_a = Genotype(size=1)
-genotype_a[0] = gene_pool_a[0][0]
+genotype_a[0] = genepool[0][0]
 
 genotype_b = Genotype(size=1)
-genotype_b[0] = gene_pool_a[2][0]
+genotype_b[0] = genepool[2][0]
 
 individual_a = Individual(sex='M', genotype=genotype_a)
-individual_b = Individual(sex='F', genotype=genotype_b)
+individual_b = Individual(sex='F', genotype=genotype_a)
+individual_c = Individual(sex='F', genotype=genotype_b)
+individual_d = Individual(sex='M', genotype=genotype_b)
 
 population = [
               [individual_a, 1000],
-              [individual_b, 1000]
+              [individual_b, 1000],
+              [individual_c, 1000],
+              [individual_d, 1000]
              ]
 
-first_gen = Generation(population=population)
+first_gen = Generation(population=population, genepool=genepool)
 
 simulation = Simulation(max_generations=150,
                         first_generation=first_gen,
